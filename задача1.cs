2b41558b4107d93a8fa685e88e585c872744cc17
Polina17 <31629944+Polina17@users.noreply.github.com>
2017-09-08 21:48:46 +0300

    Add files via upload

---
+++ задача1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int a1 = int.Parse(a);
+            int b1 = int.Parse(b);
+            var c=a1;
+            a1 = b1;
+            b1 = c;
+            a = a1.ToString();
+            b = b1.ToString();
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            
+            
+        }
+
+    }
+            
+}
