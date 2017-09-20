bf20a214937d45539711e123918ea2c831399ce9
Polina17 <pzyryanova00@mail.ru>
2017-09-21 01:13:10 +0500

    Добавила 2 задачи на семинар из 2 модуля

---
+++ Fall2017/sem/Модуль 2. Задача 2/Модуль 2. Задача 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Модуль_2.Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch = int.Parse(Console.ReadLine());   //Часы
+            int min= int.Parse(Console.ReadLine());   //Минуты
+            int k;
+            k = (Math.Abs(min - (ch%12) * 5 - min / 12)) * 6;  //На циферблате 60 чёрточек, 1 чёрточка=6 градусов. Между соседними числами 5 чёрточек, по 12 минут
+            if (k > 180)
+            {
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(360 - k);
+            }
+            else
+            {
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
